69e692c4d1eb61f873cc5063aea499905778fc11
Half-Gray <halfgray7e@gmail.com>
2024-01-24 05:18:44 -0800

    Handle Markdown parsing in application rather than relying on Reveal.js
    Reveal.js does not re-render Markdown content during a sync() call,
    and the workarounds mentioned in a GitHub issue don't seem to work.
    
    GitHub issue: https://github.com/hakimel/reveal.js/issues/2700

--- src/FilledStory.vue
+++ src/FilledStory.vue
@@ -1,23 +1,26 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 import { TextPart } from 'storyfillup';
+import { marked } from 'marked';
 import { FilledPlaceholder } from './types';
 import '@fontsource/caveat';
 
 const props = defineProps<{ story: (TextPart | FilledPlaceholder)[] }>();
-const text = computed(() =>
-	props.story
+const text = computed(() => {
+	var md = props.story
 		.map((part) =>
 			part.type === 'placeholder'
 				? `<span class="filled">${part.filled || '(' + part.description + ')'}</span>`
 				: part.content
 		)
-		.join('')
-);
+		.join('');
+	return marked.parse(md);
+});
 </script>
 
 <template>
-	<section data-markdown>{{ text }}</section>
+	<!-- eslint-disable-next-line vue/no-v-html -->
+	<section v-html="text"></section>
 </template>
 
 <style scoped>
